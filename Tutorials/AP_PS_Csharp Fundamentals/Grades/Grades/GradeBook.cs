0ea3fa2a00b2996372e9d76257e7a30644f6affd
Andy Perkins <andyperkins1978@gmail.com>
2016-12-27 23:49:45 -0500

    Back to where conflict happened

--- Tutorials/AP_PS_Csharp Fundamentals/Grades/Grades/GradeBook.cs
+++ Tutorials/AP_PS_Csharp Fundamentals/Grades/Grades/GradeBook.cs
@@ -15,6 +15,24 @@ namespace Grades
             grades = new List<float>();  // can do as a field as below or in the traditional ctor - same except here you can pass parameters in
         }
 
+
+        internal GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            // loops over the grades in the grades collection and keeps track of the highest and lowest and computes sum
+            foreach (float grade in grades)
+            {
+                // Math.max static member) pass in 2 numbers and it will return the highest of the 2
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+
         // members of the class - 2 types
         // 1.hold state or data - grades
         // 2.behavior - do work - methods - are verbs
@@ -23,6 +41,7 @@ namespace Grades
             grades.Add(grade); 
         }
 
+    
         // grades is a field - member that holds state. List is from the collection class. list of floats to hold grades
         // private set explicitly for readability - private is default access mod.  do because you want to encapsulate / protect
         private List<float> grades;             // = new List<float>(); // generic type syntax <>.
